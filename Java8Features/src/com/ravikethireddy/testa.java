2c331b923f817dfc23e00dcc1e45ab68bc431156
ravikethireddy <50884230+ravikethireddy@users.noreply.github.com>
2021-11-16 16:34:32 -0500

    Update testa.java

--- Java8Features/src/com/ravikethireddy/testa.java
+++ Java8Features/src/com/ravikethireddy/testa.java
@@ -3,5 +3,6 @@ package com.ravikethireddy;
 public class testa {
   public static void main(String[] args) {
     System.out.println("test fetch local simple pull");
+    System.out.println("latest repo");
   }
 }
